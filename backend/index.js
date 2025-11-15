bbd0a6b8e1773188bc46d5b93d13e8e5c581225d
chadunivihanga48 <vihangadmc@gmail.com>
2025-11-15 22:40:19 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.send("hii")
-})
+// MongoDB connection
+const mongoURL = process.env.MONGO_URL;
+if (!mongoURL) {
+  console.error("❌ MONGO_URI is not defined. Check your .env file!");
+  process.exit(1); // Stop server if URI is missing
+}
 
-app.listen(port, () => {
-    console.log("server started")
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.error("MongoDB connection error ❌", err));
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
